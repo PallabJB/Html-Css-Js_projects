9164055df0d7577698cd0f36ecaf4de1b398792d
PallabJB <111581851+PallabJB@users.noreply.github.com>
2024-06-06 03:34:50 +0530

    Delete E-commerce_website/vite.config.js

--- E-commerce_website/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    host: true, // This makes the server listen on all addresses, allowing access from the network
-    port: 3000, // You can specify any port you prefer
-  }
-});
